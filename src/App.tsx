371e6be646cfed9ce84525c494b8a2d19d83231b
Programmer-Manik <programmermanik1502083874@gmail.com>
2024-06-27 01:02:14 +0600

    Devtools, Middlwares And Custome Middlwares ;

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
     <div className=" h-screen w-full  flex justify-center items-center">
       <div className="flex border border-purple-300 rounded-md bg-slate-50 p-10 ">
         <button
-        onClick={() => dispatch(incrementByValue({value:5}))}
+        onClick={() => dispatch(incrementByValue(5))}
         className="px-3 py-2 rounded-md bg-green-500 text-xl font-semibold text-white">
           Increment by 5
         </button>
